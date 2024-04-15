e57abd7d24867dd16335299475b31bc22f317844
rainboowbee@gmail.com <rainboowbee@gmail.com>
2024-04-15 18:19:30 +0300

    Включил ЧПУ и настроил маршрутизацию на точку входа

--- config/web.php
+++ config/web.php
@@ -19,6 +19,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'zSPcwKmTvGUaSMVwsdDhpMQoNC9Txhrf',
+            'baseUrl' => ''
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -46,14 +47,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
