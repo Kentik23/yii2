565c9fd38e3cabe2e8324c87d8efc93636232631
rainboowbee@gmail.com <rainboowbee@gmail.com>
2024-04-15 17:23:38 +0300

    Добавил жадную загрузку данных

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
     public function actionIndex()
     {
         //$posts = Post::find()->all();
-        $query = Post::find();
+        $query = Post::find()->with('category');
         $pages = new Pagination(['totalCount' => $query -> count(), 'pageSize' => 4]);
         $posts = $query -> offset($pages->offset)->limit($pages->limit)->all();
         return $this->render('index', compact('posts', 'pages'));
