d74c773f938dd3a0878d922113c2bb5d34bad858
rainboowbee@gmail.com <rainboowbee@gmail.com>
2024-04-15 14:33:15 +0300

    Подключил ресурсы к главному шаблону

--- views/layouts/blog.php
+++ views/layouts/blog.php
@@ -1,4 +1,10 @@
+<?php
 
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,18 +12,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Front page</title>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700,800,900" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,900" rel="stylesheet">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="responsive.css">
+    <?php $this->head() ?>
 </head>
 
 <body>
+<?php $this->beginBody() ?>
 <div class="wrapper">
     <header class="header">
         <div class="container">
@@ -204,10 +203,8 @@
         </div>
     </footer>
 </div>
-<script src="js/jquery-3.1.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/owl.carousel.min.js"></script>
-<script src="js/active.js"></script>
+<?php $this->endBody() ?>
 </body>
 
 </html>
+<?php $this->endPage() ?>
